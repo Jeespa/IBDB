ad0653b6c2b1d12699f0559764a842c029518b88
Adrian Fagerland <adriafag@stud.ntnu.no>
2023-02-19 12:15:29 +0000

    (#4) Add: Search icon
    Search icon is now displayed to the right.
    It doesn't do anything other than focusing search input when clicked.
    Also fixed some React imports I suspect I messed up🫣.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState, useEffect, useRef } from 'react';
 import { db } from '../firebase-config';
 import { collection, query, where, getDocs } from 'firebase/firestore';
@@ -45,6 +45,7 @@ function Search() {
   };
 
   const searchContainerRef = useRef<HTMLDivElement>(null);
+  const searchInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -67,6 +68,13 @@ function Search() {
     setShowResults(false);
   };
 
+  const handleSearchIconClick = () => {
+    if (searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+    setShowResults(true);
+  }
+
   return (
     <div className="search-container" ref={searchContainerRef}>
       <input
@@ -74,9 +82,11 @@ function Search() {
         value={searchQuery}
         onChange={handleSearchQueryChange}
         onClick={() => setShowResults(true)}
+        ref={searchInputRef}
         className="search-input"
         placeholder="Search IBDb"
       />
+      <span className="search-icon" onClick={handleSearchIconClick}></span>
       {showResults && (
         <div className="search-results">
           <ul className="search-list">
