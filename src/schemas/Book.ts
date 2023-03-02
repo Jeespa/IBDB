00f14683f6877a0ebdfa8cbd46b64ffebb25c779
Adrian Fagerland <adriafag@stud.ntnu.no>
2023-03-02 10:29:36 +0000

    (#4) Search med bilde
    Lagt inn bilder for bøker i search.
    Forbedret search-filtrering betydelig, viser nå kun top tre søkematch for bøker.
    Oppdatert schemas for å matche bachend med authorsID.

--- src/schemas/Book.ts
+++ src/schemas/Book.ts
@@ -3,6 +3,7 @@ import { Timestamp } from 'firebase/firestore';
 export interface Book {
     documentID: string; // ISBN
     authors?: Array<string>;
+    authorsID?: Array<string>;
     title: string;
     description?: string;
     genre?: Array<string>;
