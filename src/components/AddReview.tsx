e71481ec40731ee98345b1eea1815eea75d0bdc7
jesperrg <jesperrg@stud.ntnu.no>
2023-03-06 12:11:09 +0000

    (#31) Fikset bug i addreview
    Fikset bug som gjorde at dokumentet som ble lastet opp til Firebase sin
    id ikke inneholdt brukeren som lagde reviewen.

--- src/components/AddReview.tsx
+++ src/components/AddReview.tsx
@@ -1,48 +1,42 @@
-import { Button, Container, MenuItem, Paper, Select, SelectChangeEvent, Stack, TextField } from "@mui/material";
+import {
+  Button,
+  Container,
+  MenuItem,
+  Paper,
+  Select,
+  SelectChangeEvent,
+  Stack,
+  TextField,
+} from "@mui/material";
 import { doc, setDoc } from "firebase/firestore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { auth, db } from "../firebase-config";
 
 function Review() {
-
-  const [rating, setRating] = useState('1');
+  const [rating, setRating] = useState("1");
   const [ratingValue, setRatingValue] = useState(1);
-  const [text, setText] = useState('');
-  const [user, setUser] = useState(auth.currentUser?.uid || '');
+  const [text, setText] = useState("");
   const [isbn, setIsbn] = useState(useParams().isbn);
-  const [reviewId, setReviewId] = useState(isbn + user);
   const [published, setPublished] = useState(new Date().toLocaleString() + "");
-  //Date.parse(new Date().toLocaleString() + "") / 1000
 
   const handleChange = (event: SelectChangeEvent) => {
-    setRating(event.target.value)
+    setRating(event.target.value);
     setRatingValue(parseInt(rating));
-  }
-
-  function setValues() {
-    setUser(auth.currentUser?.uid || '');
-    setReviewId(isbn + user);
-  }
+  };
 
   const addReview = async () => {
-    setValues();
+    const user = auth.currentUser?.uid;
+    const test = (isbn! + user);
     if (ratingValue > 0 && ratingValue < 7) {
       try {
-        await setDoc(doc(db, "reviews", reviewId), {
+        await setDoc(doc(db, "reviews", test), {
           isbn,
           user,
           published,
           rating,
           text,
         });
-        /* //setIsbn('');
-        //setUser(auth.currentUser?.uid);
-        setReviewId(isbn + user);
-        //setPublished('');
-        setRating('1');
-        setRatingValue(parseInt(rating));
-        setText(''); */
         alert("Review has been added!");
       } catch (e) {
         console.error("Error adding review: ", e);
@@ -50,13 +44,14 @@ function Review() {
     } else {
       alert("Fill in a rating between 1 and 6!");
     }
-  }
+  };
 
   return (
     <div>
-    <Container
+      <Container
         component={Paper}
-        sx={{ marginBottom: "20px", padding: "20px"}}>
+        sx={{ marginBottom: "20px", padding: "20px" }}
+      >
         <h2 style={{ fontSize: "20px" }}>Legg til vurdering</h2>
         <Stack direction="row" spacing={2}>
           <TextField
@@ -74,12 +69,14 @@ function Review() {
             <MenuItem value={"4"}>4</MenuItem>
             <MenuItem value={"5"}>5</MenuItem>
             <MenuItem value={"6"}>6</MenuItem>
-            </Select>
-        <Button variant="contained" onClick={addReview}>Legg til vurdering</Button>
+          </Select>
+          <Button variant="contained" onClick={addReview}>
+            Legg til vurdering
+          </Button>
         </Stack>
       </Container>
     </div>
-  )
+  );
 }
 
 export default Review;
