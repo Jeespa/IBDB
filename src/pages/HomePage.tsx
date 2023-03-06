a5c21d87e00be10fc154ed2ce0405db8feb79668
Adrian Fagerland <adriafag@stud.ntnu.no>
2023-03-06 16:55:36 +0000

    (#19) Remove: Useless Home.tsx
    Also cleanup of HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,14 @@
 import Slideshow from "../components/Slideshow";
-import { auth } from "../firebase-config";
 import "./HomePage.css"
 
 function Home() {
-  console.log(auth.currentUser);
   return (
-    <>
     <div className="topPage">
       <h1>Månedens beste bøker</h1>
       <div>
         <Slideshow />
       </div>
     </div>
-    </>
   )
 }
 
