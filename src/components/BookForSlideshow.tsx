9aec4d0746ae267e5685646e3d499833528ba69b
Adrian Fagerland <adriafag@stud.ntnu.no>
2023-03-06 17:09:52 +0000

    (#19) Fix: Norsk boktekst

--- src/components/BookForSlideshow.tsx
+++ src/components/BookForSlideshow.tsx
@@ -5,9 +5,9 @@ function BookForSlideshow ({ book }: { book: Book }) {
       <div style={{ marginTop: 10 }}>
         {/* <img style={{ marginTop: 10 }} src={"/ibdb.png"} /> */}
         <h2>{book.title}</h2>
-        <div>Description: {book.description}</div>
-        <div>Author: {book.authors}</div>
-        <div>Number of pages: {book.pages}</div>
+        <div>Beskrivelse: {book.description}</div>
+        <div>Forfattere: {book.authors?.join(', ')}</div>
+        <div>Sideantall: {book.pages}</div>
       </div>
     );
   }
