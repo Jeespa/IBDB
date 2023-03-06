96f2758c21985ae97d46f5dce7a30e6d0cfc918b
jesperrg <jesperrg@stud.ntnu.no>
2023-03-06 12:54:24 +0000

    (#19) Oppdaterte Home.tsx
    Fjernet console.log or ubrukt import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,12 @@
 import Slideshow from "../components/Slideshow";
-import { auth } from "../firebase-config";
-
 
 function Home() {
-  console.log(auth.currentUser);
   return (
-   
-
-<div> 
-<h1>Home</h1>
-  <Slideshow />
-</div>
-  )
+    <div>
+      <h1>Home</h1>
+      <Slideshow />
+    </div>
+  );
 }
 
 export default Home;
