b5e9645d6eeb8767ae141efc40a3d9abbef573b0
Jonas Rønning <jonasronning@dhcp-10-22-6-139.wlan.ntnu.no>
2023-03-06 16:00:18 +0100

    (#30) Oppdatere bokside
    Oppdatere bokside og hente bilde til boka fra database. Også nytt design på
    reviews på siden

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,25 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import { db } from "../firebase-config.js";
 import { onSnapshot, doc} from "firebase/firestore";
-import '../index.css'
+import { ref, getDownloadURL } from 'firebase/storage';
+
+import { db, storage } from "../firebase-config";
 import { Book } from '../schemas/Book'
 import AddReview from '../components/AddReview';
 import Reviews from '../components/Reviews';
 import AverageRating from '../components/AverageRating';
+import '../index.css'
+import './BookPage.css'
 
 function BookPage() {
   const { isbn } = useParams();
   
   const [book, setBook] = useState<Book>();
   const [bookExists, setBookExists] = useState(true);
+  const [imageURL, setImageURLState] = useState<string>("https://via.placeholder.com/300x300.png?text=Placeholder+Image");
 
+  const setImageURL = async (book: Book) => {
+    setImageURLState(await getDownloadURL(ref(storage, `books/${book.documentID}.jpg`)))
+    console.log(book.documentID)
+  }
   const getBook = () => {
     if (isbn !== undefined) {
       const bookRef = doc(db, 'books', isbn);
       onSnapshot(bookRef, (doc) => {
       if (doc.data()){
-        setBook(doc.data() as Book);  
+        let book = doc.data() as Book
+        setBook(book);  
+        setImageURL(book)
         setBookExists(true);
         } 
       else {
@@ -29,6 +39,18 @@ function BookPage() {
     }
   };
 
+
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const handleOpenModal = () => {
+      setIsOpen(true);
+    };
+  
+    const handleCloseModal = () => {
+      setIsOpen(false);
+    };
+
+
   useEffect(() => {
     getBook();
   }, [isbn]);
@@ -37,21 +59,38 @@ function BookPage() {
     return <h1>404 Book Not Found</h1>;
   }
 
+  
+
   return (
-    <div style={{ marginTop: 10}}>
+    <div className="page">
       {book ? (
         <>
-          <h2>{book.title}</h2>
-          <div>Description: {book.description}</div>
-          <div>Author: {book.authors?.join(', ')}</div>
-          <div>Number of pages: {book.pages}</div>
+        <div className="book">
+          <div className="bookimg">
+            <img src={imageURL} alt="Search Result Image" style={{ width: "200px", height: "300px" }} />
+          </div>
+          <div className="bookinfo">
+            <h1>{book.title}</h1>
+            <div className="author">{book.authors?.join(', ')}</div>
+            <AverageRating />
+            <div>{book.description}</div>
+            <div>Antall sider: {book.pages}</div>
+          </div>
+        </div>
+        <div className="reviewmodal">
+        <button onClick={handleOpenModal}> Legg til vurdering</button>
+          {isOpen && (
+            <div>
+              <AddReview />
+              <button onClick={handleCloseModal}> Lukk </button>
+            </div>
+          )}
+        </div>
+      <Reviews />
         </>
       ) : (
         <p>Loading...</p>
         )}
-      <AverageRating />
-      <AddReview />
-      <Reviews />
     </div>
   );
 }
