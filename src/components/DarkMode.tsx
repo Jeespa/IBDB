1aa1741d34c8ae43a1a25612f0c57e423dcc4d8d
sandrml <sandrml@stud.ntnu.no>
2023-03-13 15:42:47 +0000

    (#32) Lagt inn mindre ikon for darkmode/lightmode

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -20,7 +20,7 @@ function DarkModeToggle() {
 
   return (
     <button id='darkmode' onClick={handleToggleMode}>
-      {isDarkMode ? <img id='lightModeIcon' src="/lightmode.png" alt="Toggle Light Mode" /> : <img id='darkModeIcon' src="/darkmode.png" alt="Toggle Dark Mode" />}
+      {isDarkMode ? <img id='lightModeIcon' src="/lightmode.small.png" alt="Toggle Light Mode" /> : <img id='darkModeIcon' src="/darkmode.small.png" alt="Toggle Dark Mode" />}
     </button>
   );
 }
