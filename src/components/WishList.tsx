9dfe5771d1c7bd3482e44eac0bc4c9e80e858dd8
Adrian Fagerland <adriafag@stud.ntnu.no>
2023-03-16 23:10:39 +0000

    (#27) Fix: minor issues

--- src/components/WishList.tsx
+++ src/components/WishList.tsx
@@ -1,76 +1,73 @@
-import "./WishList.css";
-import { SetStateAction, useEffect, useState } from "react";
-import { collection, deleteDoc, doc, DocumentData, onSnapshot, query , getDocs, getDoc, CollectionReference, where} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { collection, doc, getDocs, getDoc, CollectionReference } from "firebase/firestore";
+import { TableContainer, Table, Paper, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
+
 import { db } from "../firebase-config";
-
-import { TableContainer, Table, Paper, TableHead, TableRow, TableCell, TableBody, Button } from "@mui/material";
-
-import 'firebase/firestore';
 import { Book } from "../schemas/Book";
-import BookPage from "../pages/BookPage";
+import "./WishList.css";
 
-
-
-interface Props {
+interface WishlistProps {
     userId: string;
 }
 
-function WishList(props: Props) {
-    const [rows,setRows]=useState<Book[]>([]);
+function WishList(props: WishlistProps) {
+    const [rows, setRows] = useState<Book[]>([]);
+
+    const navigate = useNavigate();
+    // HER
+    const onTableCellClick = (documentID: string) => {
+        navigate(`/book/${documentID}`);
+    };
 
     useEffect(() => {
         async function getBooks() {
-          const userDocRef = doc(db, "users", props.userId);
-          const userDoc = await getDoc(userDocRef);
-          const userData = userDoc.data() ?? {};
-          const booksRef = collection(db, "books") as CollectionReference<Book>;
-          const booksSnapshot = await getDocs<Book>(booksRef);
-          
-          let booksData = booksSnapshot.docs.map((doc) => {
-            const data = doc.data() as Record<string, any>;
-            data["documentID"] = doc.id;
-            return data as Book;
-          });
-  
-          const readBooks = userData.read || [];
-          const filteredBooks = booksData.filter((book) => {
-            return readBooks.includes(book.documentID);
-          });
-          setRows(filteredBooks);
-  
-      }
-  
-      getBooks();
+            const userDocRef = doc(db, "users", props.userId);
+            const userDoc = await getDoc(userDocRef);
+            const userData = userDoc.data() ?? {};
+            const booksRef = collection(db, "books") as CollectionReference<Book>;
+            const booksSnapshot = await getDocs<Book>(booksRef);
+
+            let booksData = booksSnapshot.docs.map((doc) => {
+                const data = doc.data() as Record<string, any>;
+                data["documentID"] = doc.id;
+                return data as Book;
+            });
+
+            const wishBooks = userData.wish || [];
+            const filteredBooks = booksData.filter((book) => {
+                return wishBooks.includes(book.documentID);
+            });
+            setRows(filteredBooks);
+        }
+
+        getBooks();
     }, [props.userId]);
-  
 
-
-    return(
+    return (
         <div className="list-container">
-            <div style={{width:"100%", margin: "50 auto" }}>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 250, maxWidth:500 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Min ønskeliste</TableCell>
-                        </TableRow>
-                    </TableHead>
-                </Table>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.documentID}>
-                        <TableCell href={`/book/${row.documentID}`}>
-                        {row.title}, {row.authors?.join(", ")}
-                        </TableCell>
-                        </TableRow>
-                    ))}
-                    
-                </TableBody>
-            </TableContainer>
+            <div style={{ width: "100%", margin: "50 auto" }}>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 250, maxWidth: 500 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Min ønskeliste</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                // HER
+                                <TableRow key={row.documentID}>
+                                    <TableCell onClick={() => onTableCellClick(row.documentID)} style={{ cursor: "pointer" }}>
+                                        {row.title}, {row.authors?.join(", ")}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
         </div>
     );
 }
 export default WishList;
-
-
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,7 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { useState } from "react";
+
 import Logout from "../components/Logout";
 import AddBook from "../components/AddBook";
 import { auth, db } from "../firebase-config";
@@ -8,39 +9,41 @@ import WishList from "../components/WishList";
 
 function ProfilePage() {
 
-    const [userId, setUserId] = useState('0');
-    const [isAdmin, setIsAdmin] = useState(false);
+  const [userId, setUserId] = useState('0');
+  const [isAdmin, setIsAdmin] = useState(false);
 
-    async function CheckIfAdmin(userId: string) {
-      const docRef = doc(db, "users", userId);
-      const docSnap = await getDoc(docRef);
-      return (
-        docSnap.get("admin")
-      );
-    };
+  async function CheckIfAdmin(userId: string) {
+    const docRef = doc(db, "users", userId);
+    const docSnap = await getDoc(docRef);
+    return (
+      docSnap.get("admin")
+    );
+  };
 
-    onAuthStateChanged(auth, async (user) => {
-        if (user) {
-          setUserId(user.uid);
-          setIsAdmin(await CheckIfAdmin(userId));
-        }
-      });
-
-    function showAddBooks() {
-      if (isAdmin) {
-        return (
-          <AddBook />
-        )
-      }
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      console.log(user.uid)
+      setUserId(user.uid);
+      setIsAdmin(await CheckIfAdmin(userId));
     }
+  });
+
+  function showAddBooks() {
+    if (isAdmin) {
+      return (
+        <AddBook />
+      )
+    }
+  }
 
   return (
     <div>
-        <h1>Bruker</h1>
-        <Logout />
-        <WishList userId={""} />
-        {showAddBooks()}
-        
+      <h1>Bruker</h1>
+      <Logout />
+      {/* HER */}
+      <WishList userId={userId} />
+      {showAddBooks()}
+
     </div>
   )
 }
