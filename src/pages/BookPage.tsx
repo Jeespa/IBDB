77a2fa06dc12454d7cb3fe94c57311caee09ec0b
Ingeborg Skaare Olssøn <ingesols@stud.ntnu.no>
2023-03-22 18:09:00 +0000

    wishlist

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import { onSnapshot, doc} from "firebase/firestore";
+import { onSnapshot, doc, getDoc, updateDoc, arrayUnion} from "firebase/firestore";
 import { ref, getDownloadURL } from 'firebase/storage';
 
-import { db, storage } from "../firebase-config";
+import { auth, db, storage } from "../firebase-config";
 import { Book } from '../schemas/Book'
 import AddReview from '../components/AddReview';
 import Reviews from '../components/Reviews';
 import AverageRating from '../components/AverageRating';
 import '../index.css'
 import './BookPage.css'
+import { onAuthStateChanged } from 'firebase/auth';
 
 function BookPage() {
   const { isbn } = useParams();
@@ -17,6 +18,8 @@ function BookPage() {
   const [book, setBook] = useState<Book>();
   const [bookExists, setBookExists] = useState(true);
   const [imageURL, setImageURLState] = useState<string>();
+  const [hasAddedWish, setHasAddedWish]=useState(false);
+  const [buttonName,setButtonName]=useState("Legg til i min ønskeliste")
 
   const setImageURL = async () => {
     setImageURLState(await getDownloadURL(ref(storage, `books/${isbn}.jpg`)));
@@ -37,6 +40,67 @@ function BookPage() {
     })
     }
   };
+
+  const handleHasAddedWish = async () => {
+    const currentUser = auth.currentUser;
+    if (!currentUser) {
+      alert("Du må logge inn for å legge til bok i Leste bøker");
+      return;
+    }
+    const userDocRef = doc(db, "users", currentUser.uid);
+    const userDocSnapshot = await getDoc(userDocRef);
+    const userData = userDocSnapshot.data();
+    const wishlist = userData?.Wishlist || [];
+    if (hasAddedWish) {
+      const updatedWishlist = wishlist.filter((isbnInWishlist: string) => isbnInWishlist !== isbn);
+      await updateDoc(userDocRef, { Wishlist: updatedWishlist }).then(() => {
+        alert("Bok fjernet fra ønskeliste.")
+        setHasAddedWish(false);
+        setButtonName("Legg til i min ønskeliste");
+        setIsOpen(false);
+      })
+        .catch((error) => {
+          alert("En feil oppstod ved fjerning av bok fra Leste bøker: " + error.message); // Set error message
+        });
+    }
+    else {
+      updateDoc(userDocRef, {
+        Wishlist: arrayUnion(isbn),
+      }).then(() => {
+        alert("Boken ble lagt til i min ønskeliste")
+        setHasAddedWish(true);
+        setButtonName("Fjern fra min ønskeliste");
+      })
+      .catch((error) => {
+        alert("En feil oppstod ved innleggelsen av boken i Leste bøker: " + error.message); // Set error message
+      });
+    }
+  }
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const userDocRef = doc(db, "users", user.uid);
+      getDoc(userDocRef)
+        .then((doc) => {
+          console.log(isbn);
+          if (doc.exists()) {
+            const data = doc.data();
+            if (data.Wishlist.includes(isbn)) {
+              setHasAddedWish(true);
+              setButtonName("Fjern fra min ønskeliste");
+            } else {
+              setHasAddedWish(false);
+              setButtonName("Legg til i min ønskeliste");
+              setIsOpen(false);
+            }
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+      });
+    }
+  });
+
 
 
     const [isOpen, setIsOpen] = useState(false);
@@ -77,14 +141,21 @@ function BookPage() {
           </div>
         </div>
         <div className="reviewmodal">
+        
         <button onClick={handleOpenModal}> Legg til vurdering</button>
+        <button onClick={handleHasAddedWish}>{buttonName}</button>
+        
+
           {isOpen && (
             <div>
               <AddReview />
               <button onClick={handleCloseModal}> Lukk </button>
             </div>
           )}
+        
         </div>
+    
+
       <Reviews />
         </>
       ) : (
